7b20f609e15ff5ce7c887291d0feea7d1a0fb2ee
Prashant Bhat <prashantbhat84@gmail.com>
2026-07-13 00:34:19 +0530

    layout issues fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -70,14 +70,8 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
-        {children}
         <Analytics />
-        <Header />
-        <main className="flex-1">{children}</main>
-        <Footer />
         <SpeedInsights />
-        {children}
-        <Analytics />
         <Header />
         <main className="flex-1">{children}</main>
         <Footer />

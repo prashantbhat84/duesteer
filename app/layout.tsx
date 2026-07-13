96c9b2f6c80bc2c2e0e38bfe16fdcc2f24f7b72c
Prashant Bhat <prashantbhat84@gmail.com>
2026-07-13 18:29:18 +0530

    Refactor layout to include Analytics component

--- app/layout.tsx
+++ app/layout.tsx
@@ -69,10 +69,8 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="flex min-h-full flex-col bg-white text-slate-900">
-        <Header />
-        <main className="flex-1">{children}</main>
-        <Footer />
+      <body className="min-h-full flex flex-col">
+        <Analytics />
         <SpeedInsights />
         <Header />
         <main className="flex-1">{children}</main>
